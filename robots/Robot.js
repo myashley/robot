b7fd558f1014a9bf9dcb78cdf660bd000dfc559b
Ashley <36495929+myashley@users.noreply.github.com>
2018-06-12 16:40:30 +1000

    Add schema and initiate timestamp

---
+++ robots/Robot.js
@@ -0,0 +1,15 @@
+const mongoose = require('../db/connection')
+
+const Schema = mongoose.Schema
+
+const robotSchema = new Schema({
+    name: String,
+    type: [String],
+    bodyColour: [String],
+    faceColour: [String],
+    highlightColour: [String],
+    personality: [String],
+    message: String,
+    dob: Date,
+    price: Number,
+})

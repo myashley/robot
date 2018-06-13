ee1e6cf30d176136d9a14f74083c072c52c09ba7
Ashley <36495929+myashley@users.noreply.github.com>
2018-06-13 11:37:25 +1000

    Edit schema

--- robots/Robot.js
+++ robots/Robot.js
@@ -1,16 +1,17 @@
 const mongoose = require('../db/connection')
-
-
+const timestamps = require('mongoose-timestamp')
 const Schema = mongoose.Schema
 
 const robotSchema = new Schema({
     name: String,
-    type: [String],
-    bodyColour: [String],
-    faceColour: [String],
-    highlightColour: [String],
-    personality: [String],
-    message: String,
+    type: String,
+    bodyColour: String,
+    faceColour: String,
+    highlightColour: String,
+    personality: String,
+    message: [String],
     dob: Date,
     price: Number,
 })
+
+robotSchema.plugin(timestamps)
